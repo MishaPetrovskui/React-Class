c29824411b957c1a2e7a850fbad163f020dc6379
Dram <154260100+MishaPetrovskui@users.noreply.github.com>
2025-12-15 18:33:25 +0000

    create clicker

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
 import "./styles.css";
 import { useState } from "react";
 
-function Hello() {
-  alert("Hello USER");
-}
+function Counted({ btn1, btn2, btn3 }) {
+  const [clicked, setClick] = useState(0);
 
-export default function App() {
-  // const [header, setHeader] = useState("My first React APP");
-  // const [inputText, setInputText] = useState("");
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
-  const [output, setOutput] = useState("");
-
-  // function changeHeader() {
-  //   setHeader(inputText);
-  // }
-  function plusMath() {
-    setOutput(Number(input1) + Number(input2));
-  }
-
-  function minusMath() {
-    setOutput(Number(input1) - Number(input2));
-  }
-
-  function multiplyMath() {
-    setOutput(Number(input1) * Number(input2));
-  }
-
-  function divideMath() {
-    setOutput(Number(input1) / Number(input2));
+  function Click(input) {
+    setClick(clicked + input);
   }
 
   return (
     <>
-      {/* <div className="App">
-        <h1>{header}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={changeHeader}>Click</button>
-      </div>
-      <div>
-        <input
-          type="text"
-          value={inputText}
-          onChange={(event) => setInputText(event.target.value)}
-        />
-        <button onClick={changeHeader}>changeHeader</button>
-      </div> */}
       <div className="App">
-        <h1>{output}</h1>
-        <input
-          type="text"
-          value={input1}
-          onChange={(event) => setInput1(event.target.value)}
-        />
-        <button onClick={plusMath}>+</button>
-        <button onClick={minusMath}>-</button>
-        <button onClick={multiplyMath}>*</button>
-        <button onClick={divideMath}>/</button>
-        <input
-          type="text"
-          value={input2}
-          onChange={(event) => setInput2(event.target.value)}
-        />
+        <h1>{clicked}</h1>
+        <button onClick={() => Click(btn1)}>{btn1}</button>
+        <button onClick={() => Click(btn2)}>{btn2}</button>
+        <button onClick={() => Click(btn3)}>{btn3}</button>
       </div>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <Counted btn1={1} btn2={-1} btn3={10} />
     </>
   );
 }
