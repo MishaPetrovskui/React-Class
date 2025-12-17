3be578e35880d7c84e16c10ccc43309653581344
Dram <154260100+MishaPetrovskui@users.noreply.github.com>
2025-12-17 16:48:27 +0000

    create color switch

--- src/App.js
+++ src/App.js
@@ -1,108 +1,28 @@
 import "./styles.css";
 import { useState } from "react";
 
-function Note({ text }) {
-  const [isEdit, setIsEdit] = useState(false);
-  const [textarea, settextArea] = useState(text);
-
-  return (
-    <>
-      <h3>Notes</h3>
-      {isEdit ? (
-        <form onSubmit={() => setIsEdit(false)}>
-          <textarea
-            value={textarea}
-            onChange={(e) => {
-              settextArea(e.target.value);
-            }}
-            required
-          ></textarea>
-          <button type="submit">Save</button>
-        </form>
-      ) : (
-        <p onClick={() => setIsEdit(true)}>{textarea}</p>
-      )}
-    </>
-  );
-}
-
-function Profile({ profile }) {
-  const [isEdit, setIsEdit] = useState(false);
-  const [profileData, setProfileData] = useState(profile);
-  // const [textName, setTextName] = useState(name);
-  // const [textEmail, setTextEmail] = useState(email);
-  // const [textGender, setTextGender] = useState(gender);
-
-  return (
-    <>
-      <h3>Profile</h3>
-      {isEdit ? (
-        <form onSubmit={() => setIsEdit(false)}>
-          <p>Name:</p>
-          <textarea
-            value={profileData.name}
-            onChange={(e) => {
-              setProfileData({
-                name: e.target.value,
-                email: profileData.email,
-                gender: profileData.gender,
-              });
-            }}
-            required
-          ></textarea>
-          <p>Email:</p>
-          <textarea
-            value={profileData.email}
-            onChange={(e) => {
-              setProfileData({
-                name: profileData.name,
-                email: e.target.value,
-                gender: profileData.gender,
-              });
-            }}
-            required
-          ></textarea>
-          <p>Gender:</p>
-          <select
-            value={profileData.gender}
-            onChange={(e) => {
-              setProfileData({
-                name: profileData.name,
-                email: profileData.email,
-                gender: e.target.value,
-              });
-            }}
-          >
-            <option>MAN</option>
-            <option>WOMAN</option>
-            <option>Vakasalewalewa</option>
-            <option>Сroissant</option>
-          </select>
-          <button type="submit">Save</button>
-        </form>
-      ) : (
-        <>
-          <p>Name: {profileData.name}</p>
-          <p>Email: {profileData.email}</p>
-          <p>Gender: {profileData.gender}</p>
-          <button onClick={() => setIsEdit(true)}>Edit</button>
-        </>
-      )}
-    </>
-  );
+function ThemeSwitch({ onClick }) {
+  return <button onClick={onClick}>Swith Theme</button>;
 }
 
 export default function App() {
+  const [theme, setTheme] = useState("darkTheme");
   return (
     <>
-      {/* <Note text="Hello world!" /> */}
-      <Profile
-        profile={{
-          name: "Admin",
-          email: "Admin@gmail.com",
-          gender: "Vakasalewalewa",
-        }}
-      />
+      <main className={theme}>
+        <ThemeSwitch
+          onClick={() =>
+            setTheme(
+              theme === "theme1"
+                ? "theme2"
+                : theme === "theme2"
+                ? "theme3"
+                : "theme1"
+            )
+          }
+        />
+        <h2>Hello world!</h2>
+      </main>
     </>
   );
 }
