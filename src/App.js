391b8fc29ba686c2b9878779401df594b0e6eccd
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2025-12-22 20:35:18 +0200

    Add image gallery functionality to App component
    Added a new component GetItemInfo to display images and descriptions. Updated App component to manage image and description input, and render a gallery of items.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 import "./styles.css";
 import { useState } from "react";
 
+function GetItemInfo({image,desc})
+{
+  console.log(image);
+  return (
+    <>
+      <div class="GallaryItemBox">
+        <img src={image} onError={(e) => {
+          e.target.src = 
+          "https://uxwing.com/wp-content/themes/uxwing/download/signs-and-symbols/error-icon.png"; }} >
+        </img>
+        <p>{desc}</p>
+      </div>
+    </>
+  );
+}
+
 export default function App() {
-  return <></>;
+  const [newImage, setNewImage] = useState("");
+  const [newDesciption, setNewDesciption] = useState("");
+  const [imageList, setImageList] = useState();
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setImageList(
+            imageList
+              ? [...imageList, <GetItemInfo image={newImage} desc={newDesciption} />]
+              : [<GetItemInfo image={newImage} desc={newDesciption} />]
+          );
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Введите ссылку на картинку"
+          value={newImage}
+          onChange={(e) => setNewImage(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Введите описание картинки"
+          value={newDesciption}
+          onChange={(e) => setNewDesciption(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <h3>Gallary</h3>
+      <div class="osnova">{imageList}</div>
+      
+    </>
+  );
 }
