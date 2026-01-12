1a8d0561f5ef1856618b61815f3db05645f421c8
Dram <154260100+MishaPetrovskui@users.noreply.github.com>
2026-01-12 16:11:53 +0000

    create lesson 7

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import "./styles.css";
 import { useState } from "react";
 
-function Counted({ btn1, btn2, btn3 }) {
-  const [clicked, setClick] = useState(0);
-
-  function Click(input) {
-    setClick(clicked + input);
-  }
-
-  return (
-    <>
-      <div className="App">
-        <h1>{clicked}</h1>
-        <button onClick={() => Click(btn1)}>{btn1}</button>
-        <button onClick={() => Click(btn2)}>{btn2}</button>
-        <button onClick={() => Click(btn3)}>{btn3}</button>
-      </div>
-    </>
-  );
-}
-
 export default function App() {
-  return (
-    <>
-      <Counted btn1={1} btn2={-1} btn3={10} />
-    </>
-  );
+  return <></>;
 }
